25c34943b117fb564a769f6836ae252ca29f2232
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 12:26:34 +0800

    Update etc1.cmake

--- cmake/3rdparty/etc1.cmake
+++ cmake/3rdparty/etc1.cmake
@@ -12,6 +12,17 @@ if( TARGET etc1 )
 	return()
 endif()
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
 file( GLOB ETC1_SOURCES ${BIMG_DIR}/3rdparty/etc1/*.cpp ${BIMG_DIR}/3rdparty/etc1/*.h )
 
 add_library( etc1 STATIC ${ETC1_SOURCES} )
