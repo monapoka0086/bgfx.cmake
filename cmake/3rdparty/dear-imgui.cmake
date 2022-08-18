0b98350eb6515f8a52ced7e36ed56a93d91ec42c
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 12:20:47 +0800

    Update dear-imgui.cmake

--- cmake/3rdparty/dear-imgui.cmake
+++ cmake/3rdparty/dear-imgui.cmake
@@ -12,6 +12,17 @@ if( TARGET dear-imgui )
 	return()
 endif()
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
 file( GLOB dear_IMGUI_SOURCES ${BGFX_DIR}/3rdparty/dear-imgui/*.cpp ${BGFX_DIR}/3rdparty/dear-imgui/*.h ${BGFX_DIR}/3rdparty/dear-imgui/*.inl )
 
 add_library( dear-imgui STATIC EXCLUDE_FROM_ALL ${dear_IMGUI_SOURCES} )
