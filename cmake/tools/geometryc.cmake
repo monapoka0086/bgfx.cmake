ad5e8c63250fa6f235e0813d277f73d1e35f4e73
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 12:47:44 +0800

    Update geometryc.cmake

--- cmake/tools/geometryc.cmake
+++ cmake/tools/geometryc.cmake
@@ -12,6 +12,18 @@ include( CMakeParseArguments )
 
 include( ${CMAKE_CURRENT_LIST_DIR}/../3rdparty/meshoptimizer.cmake )
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
+
 add_executable( geometryc ${BGFX_DIR}/tools/geometryc/geometryc.cpp )
 target_compile_definitions( geometryc PRIVATE "-D_CRT_SECURE_NO_WARNINGS" )
 set_target_properties( geometryc PROPERTIES FOLDER "bgfx/tools" )
