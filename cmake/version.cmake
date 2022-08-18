4514caba00cfda108b98ed65297c36c939a59852
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 11:31:30 +0800

    Update version.cmake

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -30,6 +30,17 @@ set(BGFX_API_VERSION ${CMAKE_MATCH_1})
 set(BGFX_REV_NUMBER ${GIT_REV_COUNT})
 set(BGFX_REV ${GIT_REV})
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
 # set project specific versions
 set(PROJECT_VERSION 1.${BGFX_API_VERSION}.${BGFX_REV_NUMBER})
 set(PROJECT_VERSION_MAJOR 1)
