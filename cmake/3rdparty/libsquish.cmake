6a762d34ec3e18b65449505fee43e326a4ff2c47
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 12:35:23 +0800

    Update libsquish.cmake

--- cmake/3rdparty/libsquish.cmake
+++ cmake/3rdparty/libsquish.cmake
@@ -12,6 +12,17 @@ if( TARGET squish )
 	return()
 endif()
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
 file( GLOB SQUISH_SOURCES ${BIMG_DIR}/3rdparty/libsquish/*.cpp ${BIMG_DIR}/3rdparty/libsquish/*.h ${BIMG_DIR}/3rdparty/libsquish/*.inl )
 
 add_library( squish STATIC ${SQUISH_SOURCES} )
