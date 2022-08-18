9dbbfca8779898f0ffe81cf0b4f3073ebc9d816e
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 12:46:28 +0800

    Update webgpu.cmake

--- cmake/3rdparty/webgpu.cmake
+++ cmake/3rdparty/webgpu.cmake
@@ -12,6 +12,17 @@ if( TARGET webgpu )
 	return()
 endif()
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
 file(
 	GLOB
     WEBGPU_SOURCES
