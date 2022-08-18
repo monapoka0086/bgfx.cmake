c1db73edf3237c6f13d02ea6761dd0d0738643a3
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 13:04:31 +0800

    Update texturev.cmake

--- cmake/tools/texturev.cmake
+++ cmake/tools/texturev.cmake
@@ -10,6 +10,17 @@
 
 include( CMakeParseArguments )
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
 add_executable( texturev ${BGFX_DIR}/tools/texturev/texturev.cpp )
 set_target_properties( texturev PROPERTIES FOLDER "bgfx/tools" )
 target_link_libraries( texturev example-common )
