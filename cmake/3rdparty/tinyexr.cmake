89163eeb739f7b90d0fe895b83c0e937af7058ac
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 12:45:59 +0800

    Update tinyexr.cmake

--- cmake/3rdparty/tinyexr.cmake
+++ cmake/3rdparty/tinyexr.cmake
@@ -2,6 +2,17 @@ if( TARGET tinyexr )
 	return()
 endif()
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
 file( GLOB_RECURSE TINYEXR_SOURCES ${BIMG_DIR}/3rdparty/tinyexr/*.c ${BIMG_DIR}/3rdparty/tinyexr/*.h )
 
 add_library( tinyexr STATIC ${TINYEXR_SOURCES} )
