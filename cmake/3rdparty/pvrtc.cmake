8aa6de1e3cf1a7e950d304e22f21ceeddcf81539
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 12:45:11 +0800

    Update pvrtc.cmake

--- cmake/3rdparty/pvrtc.cmake
+++ cmake/3rdparty/pvrtc.cmake
@@ -12,6 +12,18 @@ if( TARGET pvrtc )
 	return()
 endif()
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
+
 file( GLOB PVRTC_SOURCES ${BIMG_DIR}/3rdparty/pvrtc/*.cpp ${BIMG_DIR}/3rdparty/pvrtc/*.h )
 
 add_library( pvrtc STATIC ${PVRTC_SOURCES} )
