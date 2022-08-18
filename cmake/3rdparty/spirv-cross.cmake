78a9b7d531cc708fdd8036ffeecf89e4e24069e7
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 12:45:32 +0800

    Update spirv-cross.cmake

--- cmake/3rdparty/spirv-cross.cmake
+++ cmake/3rdparty/spirv-cross.cmake
@@ -12,6 +12,18 @@ if( TARGET spirv-cross )
 	return()
 endif()
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
+
 file(
 	GLOB
 	SPIRV_CROSS_SOURCES
