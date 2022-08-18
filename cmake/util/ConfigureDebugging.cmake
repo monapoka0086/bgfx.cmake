8885a11699278712ecf8be11f204c2eb3ecab782
monapoka0086 <111445908+monapoka0086@users.noreply.github.com>
2022-08-18 13:04:56 +0800

    Update ConfigureDebugging.cmake

--- cmake/util/ConfigureDebugging.cmake
+++ cmake/util/ConfigureDebugging.cmake
@@ -38,6 +38,17 @@
 
 include( CMakeParseArguments )
 
+if (MSVC)
+    add_compile_options("/MT")
+    foreach(flag_var
+        CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO)
+      if(${flag_var} MATCHES "/MD")
+        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif()
+    endforeach()
+endif()
+
 function( configure_debugging ARG_TARGET )
 	if( MSVC )
 		# Visual Studio Options
